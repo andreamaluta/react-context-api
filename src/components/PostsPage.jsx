ba16e8914183b01707b4b0a6e90a4967126c3222
andreamaluta <andreamaluta@gmail.com>
2025-05-19 15:39:50 +0200

    [UPD] added a title

--- src/components/PostsPage.jsx
+++ src/components/PostsPage.jsx
@@ -4,6 +4,7 @@ import PostsList from './PostsList'
 const PostsPage = ({ posts }) => {
     return (
         <div>
+            <h1>Lista dei post e dei contenuti</h1>
             <PostsList posts={posts} />
         </div>
     )
